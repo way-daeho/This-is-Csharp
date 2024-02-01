6c8e0a8c50a5ecbc008782d61ea4e4b0a7368c5b
way-daeho <lavr2755@gmail.com>
2024-02-01 12:12:05 +0900

    Start C# study

---
+++ Hello/MainApp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using static System.Console;
+
+namespace Hello
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            // 프로그램 실행이 시작되는 곳
+            if (args.Length == 0) // .은 ~의 로 해석하는게 이해하기 편함.
+            {
+                Console.WriteLine("사용법 : Hello.exe <이름>");
+                return;
+            }
+
+            WriteLine("Hello, {0}", args[0]); // Hello, World를 프롬프트에 출력
+        }
+    }
+}
